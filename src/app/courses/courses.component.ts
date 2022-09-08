20962f7cae5a96e93e1bd82644dd7f1c38921c69
ProgSaleh <progsaleh@hotmail.com>
2022-09-08 23:04:25 +0300

    temp

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
   courses = [
@@ -12,13 +12,28 @@ export class CoursesComponent implements OnInit {
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'typeScript Solid Fundamentals',
+      description: 'Learn the hard parts of Typescript',
+      percentComplete: 26,
+      favorite: true,
+    },
   ];
 
-  constructor() { }
+  selectedCourse = null;
 
-  ngOnInit(): void {
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
 
+  deleteCourse(courseId: number) {
+    console.log('deleted course', courseId);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
